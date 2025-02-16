e46cd4e201f69eee5102818f48c9c408ce97e235
Lucas Vitor Ferreira Santos <funcraftact4@outlook.com>
2025-02-16 20:05:09 -0300

    Transfer dates

---
+++ README.md
@@ -0,0 +1,2 @@
+# educational-page-creation-class
+ A project to be presented in an interactive class format at the rural federal college of Pernambuco
---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+1 - Usa document para manipular/buscar o DOM(Página HTML)
+2 - Busca a TAG HTML que utiliza o ID
+3 - Filtra por eventos(ações) de "click"
+4 - Engata uma função diretamente
+5 - Define um hash(variavel{const} do tipo lista{[valor1, valor2]}) de cores hexadecimais
+6 - Manipula o DOM buscando a tag <body> no HTML e mudando o estilo(cor de fundo) randomicamente ao clicar
+*/
+document.getElementById("changeColorBtn").addEventListener("click", function() {
+    const colors = ["#ffb6b9", "#ffdfba", "#ffffba", "#baffc9", "#bae1ff"];
+    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+});
